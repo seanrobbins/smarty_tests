8c26ea7b5c9fdecf9c6f8ad20208d6552d5d8cad
Sean Robbins <sean.m.robbins@gmail.com>
2018-08-07 17:52:20 +0100

    Begin defining choose_plan_steps

---
+++ features/step_definitions/choose_plan_steps.rb
@@ -0,0 +1,16 @@
+Given("I am on the Smarty homepage") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When("I select the small plan") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When("confirm I want the small plan") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I am directed to order page for signing up to the small plan") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
